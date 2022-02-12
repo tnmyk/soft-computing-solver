dcef14b9f0eb53db5df2fb1584b0a2f9f4d36ec5
Tanmay-temp <tanmaykachroo@gmail.com>
2022-02-12 19:58:09 +0530

    Added Home page headings

---
+++ components/styles/styledHome.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StyledHome = styled.div`
+  padding: 6rem 8%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const MainHeading = styled.h1`
+  font-size: 3.5rem;
+`;
+
+export const Description = styled.p`
+  font-size: 1.3rem;
+  margin-top: 0.5rem;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from "next";
+import { Input } from "../components/Input/Input";
+import {
+  Description,
+  MainHeading,
+  StyledHome,
+} from "../components/styles/styledHome";
 
 const Home: NextPage = () => {
-  return <div>Home</div>;
+  return (
+    <StyledHome>
+      <MainHeading>Soft Computing Solver</MainHeading>
+      <Description>Solve Soft Computing problems online </Description>
+    </StyledHome>
+  );
 };
 
 export default Home;
