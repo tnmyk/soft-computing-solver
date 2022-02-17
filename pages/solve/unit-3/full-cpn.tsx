2b6910f0376809385a7c81f36c4ab2a6dc793c78
Tanmaykk <tanmaykachroo@gmail.com>
2022-02-18 01:35:44 +0530

    Added inputs for W,V

--- pages/solve/unit-3/full-cpn.tsx
+++ pages/solve/unit-3/full-cpn.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Input } from "../../../components/Input/Input";
 import { StyledSolver } from "../../../components/styles/styledSolver";
 
@@ -6,7 +6,14 @@ const SolvePage = () => {
   const [numberOfX, setNumberOfX] = useState(0);
   const [numberOfZ, setNumberOfZ] = useState(0);
   const [numberOfY, setNumberOfY] = useState(0);
-
+  const [V, setV] = useState<string[]>([]);
+  const [W, setW] = useState<string[]>([]);
+  useEffect(() => {
+    setV(Array(numberOfX).fill(""));
+  }, [numberOfX]);
+  useEffect(() => {
+    setW(Array(numberOfY).fill(""));
+  }, [numberOfY]);
   return (
     <StyledSolver>
       <h1>Full CPN solver</h1>
@@ -34,8 +41,35 @@ const SolvePage = () => {
           else setNumberOfY(0);
         }}
       />
-      {new Array(numberOfX).fill(0).map((_x) => {
-        return <Input placeholder={Array(numberOfZ).fill(0).join(" ")} />;
+      {V.map((_, xIndex) => {
+        return (
+          <Input
+            placeholder={Array(numberOfZ)
+              .fill(0)
+              .map((_, zIndex) => `x${xIndex + 1}z${zIndex + 1}`)
+              .join("   ")}
+            onChange={(e) => {
+              const tempArr = [...V];
+              tempArr[xIndex] = e.target.value;
+              setV(tempArr);
+            }}
+          />
+        );
+      })}
+      {W.map((_, yIndex) => {
+        return (
+          <Input
+            placeholder={Array(numberOfZ)
+              .fill(0)
+              .map((_, zIndex) => `y${yIndex + 1}z${zIndex + 1}`)
+              .join("   ")}
+            onChange={(e) => {
+              const tempArr = [...W];
+              tempArr[yIndex] = e.target.value;
+              setW(tempArr);
+            }}
+          />
+        );
       })}
     </StyledSolver>
   );
