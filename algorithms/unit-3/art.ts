4644cf5df6c94803fddb0126968f05543e331a03
Tanmaykk <tanmaykachroo@gmail.com>
2022-03-02 01:32:09 +0530

    Steps added

--- algorithms/unit-3/art.ts
+++ algorithms/unit-3/art.ts
@@ -7,6 +7,8 @@ const solve = (
   bottomUpWeights: string,
   topDownWeights: string
 ) => {
+  const steps = [];
+  if (alpha === 0) alpha = 2;
   const getNorm = (s: number[]) => {
     return s.reduce((prv, crt) => prv + crt);
   };
@@ -16,22 +18,29 @@ const solve = (
     n: number,
     m: number
   ): number[] => {
+    let tempSteps = Array(m).fill("");
     let yArr: number[] = Array(m).fill(0);
     for (let i = 0; i < n; ++i) {
       for (let j = 0; j < m; ++j) {
+        tempSteps[j] += `+ ${bArr[i][j]}(${s[i]})`;
         yArr[j] += bArr[i][j] * s[i];
       }
     }
+    tempSteps = tempSteps.map(
+      (stp, idx) => `y<sub>${idx + 1}</sub> = ${stp} = ${yArr[idx]}`
+    );
+    steps.push(...tempSteps);
     return yArr;
   };
   const inputArr = inputStringArr.map((inputStr) =>
     inputStr.split(" ").map((inn) => Number(inn))
   );
+  const bij0 = 1 / (1 + n);
   const bArr =
     bottomUpWeights.trim() === ""
       ? Array(n)
           .fill(0)
-          .map(() => Array(m).fill(1 / (1 + n)))
+          .map(() => Array(m).fill(bij0))
       : bottomUpWeights
           .trim()
           .split("\n")
@@ -55,11 +64,31 @@ const solve = (
               .split(" ")
               .map((inn) => Number(inn))
           );
+  steps.push(
+    `The values assumed in this case are p = ${p}, α=${alpha}. Also, it can be noted that n = ${n} and m = ${m}. Hence,`
+  );
+  steps.push(
+    `Bottom-up-weights, b<sub>ij</sub>(0) = 1/(1+n) = 1/${n + 1} = ${bij0}`
+  );
+  steps.push(`Top-down-weights t<sub>ji</sub>(0) = 1`);
+
   for (let inputNo = 0; inputNo < inputArr.length; ++inputNo) {
     // replace 1 with n
-    console.log({ inputNo });
+    steps.push(
+      `For the input vector-${inputNo + 1} [${inputArr[inputNo].join(
+        " "
+      )}], s = [${inputArr[inputNo].join(" ")}]`
+    );
     const snorm = getNorm(inputArr[inputNo]);
-    console.log({ snorm });
+    steps.push(
+      `Norm of s =  ||s|| = ${inputArr[inputNo].join(
+        " + "
+      )} = ${snorm}\nCompute activations\nx = [${inputArr[inputNo].join(" ")}]`
+    );
+    steps.push(
+      `Compute net input to each node in the layer:\ny<sub>j</sub> = &sum;<sub>i=1</sub><sup>${n}</sup> b<sub>ij</sub>x<sub>i</sub>\nFor j = 1 to ${m},`
+    );
+
     const yArr = [
       ...Array.from(
         new Set(getNetInputsToEachNode(inputArr[inputNo], bArr, n, m))
