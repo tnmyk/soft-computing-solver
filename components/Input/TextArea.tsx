fb83f75cfd359f239181c6d57cbaa8743c5d97f8
Tanmaykk <tanmaykachroo@gmail.com>
2022-03-03 17:46:44 +0530

    UI tweaks (dark mode)

--- components/Input/TextArea.tsx
+++ components/Input/TextArea.tsx
@@ -7,9 +7,9 @@ export const TextArea = styled.textarea<Props>`
   font-size: ${(p) => (p.fontSize ? p.fontSize : "1rem")};
   padding: 15px;
   border: none;
-  box-shadow: 3px 4px 4px rgba(0, 0, 0, 0.06);
+  /* box-shadow: 3px 4px 4px rgba(0, 0, 0, 0.06); */
   border-radius: 7px;
-  background: #f4f4f4;
-  border: 1px solid #c8c8c8;
+  background: #2d3031;
+  border: 1px solid #525759;
   width: ${(p) => p.width};
 `;
